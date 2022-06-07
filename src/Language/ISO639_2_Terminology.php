efa00109e213c4ce3080f1fed01c544169c8fa55
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-06-07 20:32:14 +0200

    Add base language classes

---
+++ src/Language/ISO639_2_Terminology.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PrinsFrank\Standards\Language;
+
+/**
+ * @source https://www.loc.gov/standards/iso639-2/php/code_list.php
+ */
+enum ISO639_2_Terminology: string
+{
+    case Albanian = 'sqi';
+    case Armenian = 'hye';
+    case Basque = 'eus';
+    case Burmese = 'mya';
+    case Chinese = 'zho';
+    case Czech = 'ces';
+    case Dutch_Flemish = 'nld';
+    case French = 'fra';
+    case Georgian = 'kat';
+    case German = 'deu';
+    case Greek_Modern_1453 = 'ell';
+    case Icelandic = 'isl';
+    case Macedonian = 'mkd';
+    case Malay = 'msa';
+    case Maori = 'mri';
+    case Persian = 'fas';
+    case Romanian_Moldavian_Moldovan = 'ron';
+    case Slovak = 'slk';
+    case Tibetan = 'bod';
+    case Welsh = 'cym';
+}
