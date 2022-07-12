de0987729ceacec425a258e7f61b494fca2d7e5c
Viktor Szépe <viktor@szepe.net>
2022-07-12 21:00:45 +0000

    Update src/BackedEnum.php

--- src/BackedEnum.php
+++ src/BackedEnum.php
@@ -23,6 +23,6 @@ class BackedEnum
      */
     public static function tryFromKey(string $fqn, string $keyName): ?\BackedEnum
     {
-        return defined("{$fqn}::{$keyName}") ? constant("{$fqn}::{$keyName}") : null;
+        return defined("$fqn::$keyName") ? constant("$fqn::$keyName") : null;
     }
 }
