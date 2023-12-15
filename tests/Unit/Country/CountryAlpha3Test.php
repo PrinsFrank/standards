e2f2a9e5284d60ae28333a2f2fbaafe8b52788a7
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2023-12-15 17:41:34 +0100

    Csfixer

--- tests/Unit/Country/CountryAlpha3Test.php
+++ tests/Unit/Country/CountryAlpha3Test.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace PrinsFrank\Standards\Tests\Unit\Country;
 
 use PHPUnit\Framework\TestCase;
-use PrinsFrank\Standards\Country\CountryAlpha2;
 use PrinsFrank\Standards\Country\CountryAlpha3;
 use PrinsFrank\Standards\Country\Groups\EFTA;
 use PrinsFrank\Standards\Country\Groups\EU;
