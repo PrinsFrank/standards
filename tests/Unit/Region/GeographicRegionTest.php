34c9acff2cb4fd9d8c3488335315406ec60a289e
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-01-04 17:21:52 +0100

    Fix PSR4 issues (#138)

--- tests/Unit/Region/GeographicRegionTest.php
+++ tests/Unit/Region/GeographicRegionTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace PrinsFrank\Standards\Tests\Region;
+namespace PrinsFrank\Standards\Tests\Unit\Region;
 
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\Standards\Country\CountryNumeric;
