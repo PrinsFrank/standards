39679617f862c5d5fe92b9e66a3b7d5082ab3e1e
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-04 18:08:52 +0100

    Fix stub file

---
+++ dev/stubs/FacebookWebDriverWebDriverElement.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Facebook\WebDriver;
+
+interface WebDriverElement extends WebDriverSearchContext
+{
+}
---
+++ dev/stubs/SymfonyComponentDomCrawler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Symfony\Component\DomCrawler;
+
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+/**
+ * Crawler eases navigation of a list of \DOMNode objects.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ *
+ * @implements \IteratorAggregate<int, \DOMNode>
+ */
+class Crawler implements \Countable, \IteratorAggregate
+{
+}
