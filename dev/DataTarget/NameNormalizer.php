a741bfb45316c5134e9572fb11b9c3732d5ce2d0
Viktor Szépe <viktor@szepe.net>
2024-01-07 18:18:02 +0000

    Transliterate one more IPA character

--- dev/DataTarget/NameNormalizer.php
+++ dev/DataTarget/NameNormalizer.php
@@ -15,7 +15,7 @@ class NameNormalizer
             throw new TransliterationException();
         }
 
-        $key = str_replace(['ʌ'], ['e'], $key);
+        $key = str_replace(['ɔ', 'ʌ'], ['O', 'e'], $key);
         $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~', '´'], '_', $key);
 
         return trim(str_replace(['__', '__'], ['_', '_'], $key), '_');
