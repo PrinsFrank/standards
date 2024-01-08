a189ebe04f43057754476a720d0c08ca724fc143
Viktor Szépe <viktor@szepe.net>
2024-01-08 17:09:51 +0100

    Add Supplemental IPA transliteration

--- dev/DataTarget/NameNormalizer.php
+++ dev/DataTarget/NameNormalizer.php
@@ -15,7 +15,10 @@ class NameNormalizer
             throw new TransliterationException();
         }
 
-        $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~', '´'], '_', $key);
+        // Supplemental IPA transliteration
+        $key = str_replace(['´', 'Ɔ'], ['', 'O'], $key);
+
+        $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~'], '_', $key);
         if (preg_match('/[^0-9A-Z_a-z]/', $key) === 1) {
             throw new TransliterationException(sprintf('Invalid key: (%s)', $key));
         }
