36c06a363cd987726360071cdbb0659185760a93
Viktor Szépe <viktor@szepe.net>
2024-01-09 12:31:50 +0100

    Check key in NameNormalizer (#136)

--- dev/DataTarget/NameNormalizer.php
+++ dev/DataTarget/NameNormalizer.php
@@ -15,7 +15,13 @@ class NameNormalizer
             throw new TransliterationException();
         }
 
-        $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~', '´'], '_', $key);
+        // Supplemental IPA transliteration
+        $key = str_replace(['´', 'Ɔ'], ['', 'O'], $key);
+
+        $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~'], '_', $key);
+        if (preg_match('/[^0-9A-Z_a-z]/', $key) === 1) {
+            throw new TransliterationException(sprintf('Invalid key: (%s)', $key));
+        }
 
         return trim(str_replace(['__', '__'], ['_', '_'], $key), '_');
     }
