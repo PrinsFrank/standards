3074a8e4cc5fc8f5e70f520209e9f2f6d43980a0
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-28 23:31:30 +0100

    Remove rogue newline

--- dev/DataTarget/EnumFile.php
+++ dev/DataTarget/EnumFile.php
@@ -92,7 +92,7 @@ class EnumFile
         }
 
         $firstMethodPos = mb_strpos($enumContent, ' public ');
-        $newEnumContent = mb_substr($enumContent, 0, $startEnum + 1) . PHP_EOL;
+        $newEnumContent = mb_substr($enumContent, 0, $startEnum + 1);
         $cases = array_unique($this->cases);
         usort($cases, $sorting);
         foreach ($cases as $case) {
