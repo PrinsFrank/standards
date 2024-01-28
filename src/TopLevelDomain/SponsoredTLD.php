d49d508dc64e73048d2c17e15a49fc28d04e432f
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-28 22:59:52 +0100

    Add enum content

--- src/TopLevelDomain/SponsoredTLD.php
+++ src/TopLevelDomain/SponsoredTLD.php
@@ -5,4 +5,18 @@ namespace PrinsFrank\Standards\TopLevelDomain;
 
 enum SponsoredTLD: string
 {
+    case aero = 'aero';
+    case asia = 'asia';
+    case cat = 'cat';
+    case coop = 'coop';
+    case edu = 'edu';
+    case gov = 'gov';
+    case int = 'int';
+    case jobs = 'jobs';
+    case mil = 'mil';
+    case museum = 'museum';
+    case post = 'post';
+    case tel = 'tel';
+    case travel = 'travel';
+    case xxx = 'xxx';
 }
