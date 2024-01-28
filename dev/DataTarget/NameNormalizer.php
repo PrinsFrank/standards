430ed5c67ecc458c2ccab38f0956d90d6e57546d
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-01-28 21:36:15 +0100

    Use updated transliterator

--- dev/DataTarget/NameNormalizer.php
+++ dev/DataTarget/NameNormalizer.php
@@ -4,22 +4,50 @@ declare(strict_types=1);
 namespace PrinsFrank\Standards\Dev\DataTarget;
 
 use PrinsFrank\Standards\Dev\Exception\TransliterationException;
+use PrinsFrank\Transliteration\Exception\InvalidArgumentException;
+use PrinsFrank\Transliteration\Exception\UnableToCreateTransliteratorException;
+use PrinsFrank\Transliteration\FormalIdSyntax\Components\Character;
+use PrinsFrank\Transliteration\FormalIdSyntax\Components\Filter;
 use PrinsFrank\Transliteration\TransliteratorBuilder;
 
 class NameNormalizer
 {
-    /** @throws TransliterationException */
+    /**
+     * @throws TransliterationException
+     * @throws InvalidArgumentException
+     * @throws UnableToCreateTransliteratorException
+     */
     public static function normalize(string $key): string
     {
         $key = (new TransliteratorBuilder())
             ->toASCII()
-            ->toXSampa()
+            ->replace(' ', '_')
+            ->replace(';', '_')
+            ->replace(',', '_')
+            ->replace('(', '_')
+            ->replace(')', '_')
+            ->replace('-', '_')
+            ->replace('.', '_')
+            ->replace('\'', '_')
+            ->replace('"', '_')
+            ->replace('/', '_')
+            ->replace('|', '_')
+            ->replace('=', '_')
+            ->replace('!', '_')
+            ->replace('?', '_')
+            ->replace('*', '_')
+            ->replace('[', '_')
+            ->replace(']', '_')
+            ->replace('~', '_')
+            ->IPAToEnglishApproximation()
+            ->keep(
+                (new Filter())
+                    ->addRange(new Character('a'), new Character('z'))
+                    ->addRange(new Character('A'), new Character('Z'))
+                    ->addChar(new Character(' '))
+            )
             ->transliterate($key);
 
-        // Supplemental IPA transliteration
-        $key = str_replace(['´', 'Ɔ'], ['', 'O'], $key);
-
-        $key = str_replace([' ', ';', ',', '(', ')', '-', '.', '\'', '"', '/', '|', '=', '!', '?', '*', '[', ']', '~'], '_', $key);
         if (preg_match('/[^0-9A-Z_a-z]/', $key) === 1) {
             throw new TransliterationException(sprintf('Invalid key: (%s)', $key));
         }
