fd9edb65bb18c093af92c9a39ac03cbc363c44f7
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-11-18 21:29:56 +0000

    Add temporary assignment methods to HTTP Status code enum (#276)

---
+++ tests/Unit/Http/HttpStatusCodeTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace PrinsFrank\Standards\Tests\Unit\Http;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\Standards\Http\HttpStatusCode;
+
+#[CoversClass(HttpStatusCode::class)]
+class HttpStatusCodeTest extends TestCase
+{
+    public function testIsTemporaryAssignment(): void
+    {
+        static::assertFalse(HttpStatusCode::OK->isTemporaryAssignment());
+        static::assertTrue(HttpStatusCode::Upload_Resumption_Supported->isTemporaryAssignment());
+    }
+
+    public function testGetTemporaryAssignmentExpiresAt(): void
+    {
+        static::assertNull(HttpStatusCode::OK->getTemporaryAssignmentExpiresAt());
+        static::assertEquals(
+            new DateTimeImmutable('2025-11-13'),
+            HttpStatusCode::Upload_Resumption_Supported->getTemporaryAssignmentExpiresAt()
+        );
+    }
+}
