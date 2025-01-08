dff3b75c27859fb5722b74a5df0932d15339515b
Viktor Szépe <viktor@szepe.net>
2025-01-08 21:34:54 +0000

    Add missing SponsoredTLD

--- src/TopLevelDomain/AnyTLD.php
+++ src/TopLevelDomain/AnyTLD.php
@@ -12,16 +12,17 @@ use ValueError;
  */
 class AnyTLD {
     /** @throws ValueError */
-    public static function from(string $string): CountryCodeTLD|GenericRestrictedTLD|GenericTLD|InfrastructureTLD|TestTLD {
+    public static function from(string $string): CountryCodeTLD|GenericRestrictedTLD|GenericTLD|InfrastructureTLD|SponsoredTLD|TestTLD {
         return self::tryFrom($string)
             ?? throw new ValueError(sprintf('"%s" is not a valid backing value for any TLD enum', $string));
     }
 
-    public static function tryFrom(string $string): CountryCodeTLD|GenericRestrictedTLD|GenericTLD|InfrastructureTLD|TestTLD|null {
+    public static function tryFrom(string $string): CountryCodeTLD|GenericRestrictedTLD|GenericTLD|InfrastructureTLD|SponsoredTLD|TestTLD|null {
         return CountryCodeTLD::tryFrom($string)
             ?? GenericRestrictedTLD::tryFrom($string)
             ?? GenericTLD::tryFrom($string)
             ?? InfrastructureTLD::tryFrom($string)
+            ?? SponsoredTLD::tryFrom($string);
             ?? TestTLD::tryFrom($string);
     }
 }
