e6941ff20c0fe93ff0f41d9196e3b4ea8e9bd4ed
MartkCz <martk494@gmail.com>
2025-11-06 23:22:20 +0100

    Add Rand Cent (ZAc) as a minor currency unit (#349)

--- src/Currency/MinorUnits/CurrencyMinorUpperXAlpha3.php
+++ src/Currency/MinorUnits/CurrencyMinorUpperXAlpha3.php
@@ -11,12 +11,14 @@ enum CurrencyMinorUpperXAlpha3: string {
     case Penny_Sterling = 'GBX';
     case US_Cent = 'USX';
     case Euro_Cent = 'EUX';
+    case Rand_Cent = 'ZAX';
 
     public function toCurrencyMinorLowerLastAlpha3(): CurrencyMinorLowerLastAlpha3 {
         return match ($this) {
             self::Penny_Sterling => CurrencyMinorLowerLastAlpha3::Penny_Sterling,
             self::US_Cent => CurrencyMinorLowerLastAlpha3::US_Cent,
             self::Euro_Cent => CurrencyMinorLowerLastAlpha3::Euro_Cent,
+            self::Rand_Cent => CurrencyMinorLowerLastAlpha3::Rand_Cent,
         };
     }
 }
